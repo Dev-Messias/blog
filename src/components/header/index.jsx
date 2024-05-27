f6a3fc496512518c30d9015eea45a2f4c38a6a95
DevMessias <messiasbento944@gmail.com>
2024-05-27 14:04:57 -0300

    Feat:  Componente Header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,26 @@
+import { IoMenu } from "react-icons/io5";
+
+function Header() {
+ return (
+   <header className="w-full flex flex-row items-center justify-between   px-4 py-4 md:py-10 lg:px-32 absolute" >
+        <div className=" w-full flex flex-row items-center justify-between"  >
+            <h1 className="font-bold text-2xl md:text-4xl text-gray-100" >Blogr</h1>
+            <IoMenu className="block md:hidden text-3xl text-gray-100" />
+            <div className="hidden w-full md:flex flex-row justify-between items-center" >
+              <nav className="flex  flex-row items-center justify-center gap-5 ml-20"  >
+                <a className="text-md font-light text-gray-50"  href="#">Product</a>
+                <a className="text-md font-light text-gray-50" href="#">Company</a>
+                <a className="text-md font-light text-gray-50" href="#">Connect</a>
+              </nav>
+              <div className="flex  flex-row items-center justify-center gap-3 " >
+                <button className="bg-transparent py-2 px-8 text-gray-50" >Login</button>
+                <button className="bg-gray-50 py-2 font-bold px-8 rounded-full text-red-400" >Sign Up</button>
+              </div>
+            </div>
+        </div>
+
+   </header>
+  );
+}
+
+export default Header;
---
+++ src/components/sectionInicial/index.jsx
@@ -0,0 +1,24 @@
+
+ function SectionInicial() {
+    return (
+        <section class="w-full h-screen rounded-bl-[120px] flex items-center justify-center bg-gradient-to-r from-red-400 to-red-400" >
+            <div className=" w-full h-screen bg-imgInicio bg-cover md:bg-auto bg-no-repeat bg-left flex flex-col items-center justify-center " >
+
+
+                <div className=" w-full flex flex-col items-center justify-center " >
+
+                    <div className=" w-full flex flex-col items-center justify-center" >
+                        <h2 className="text-center w-80 md:w-auto font-semibold text-4xl md:text-7xl text-gray-50" >A modern publishing platform</h2>
+                        <p className="text-gray-50 text-lg font-light md:text-xl mt-6" >Grow your audince and build your online brand</p>
+                    </div>
+                    <div className="flex flex-row justify-center items-center gap-6 mt-20" >
+                        <button className="bg-gray-50 px-7 py-3 rounded-full text-red-400 font-bold text-lg " >Start for Free</button>
+                        <button className="bg-transparent border-2 px-7 py-3 rounded-full text-gray-50 font-bold text-lg" >Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default SectionInicial;
